bcfe369e0fab97260e2289967448a90f3029c741
Marsyew <yew0024@163.com>
2024-08-15 10:26:38 +0800

    a

--- examples/pipelines/integrations/comfyui_query.py
+++ examples/pipelines/integrations/comfyui_query.py
@@ -23,10 +23,12 @@ class Pipeline:
     async def on_startup(self):
         # This function is called when the server is started.
         print(f"on_startup:{__name__}")
+        pass
 
     async def on_shutdown(self):
         # This function is called when the server is stopped.
         print(f"on_shutdown:{__name__}")
+        pass
 
     def pipe(
         self, user_message: str, model_id: str, messages: List[dict], body: dict
@@ -40,7 +42,7 @@ class Pipeline:
         }
 
         data = {
-            'model': model_id,
+            'model': test,
             'messages': messages
         }
 
